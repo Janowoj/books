19369d4d7a8172447edc17ff3a5b4e2df9dad15f
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-09-10 23:51:36 +0200

    how to update an array or object which is managed by our state system (review)

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import BookCreate from "./components/BookCreate";
 
 function App() {
 
-    const [book, setBook] = useState([]);
+    const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log("I would like to read:", title);
+        // console.log("I would like to read:", title);
+        books.push({ id: 123, title: title });
+        console.log(books);
+        setBooks(books);
     }
 
     return <div>
+        {books.length}
         <BookCreate onCreate={createBook} />
     </div>
 }
 
 export default App;
+
+// BAD SOLUTION to update the state of books:
+
+// const createBook = (title) => {
+//     books.push({ id: 123, title: title });
+//     setBooks(books);
+// }
+
+// initially books.length is equal 0, but after adding some title nothing changes!
