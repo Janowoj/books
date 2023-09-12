c92aa889f8d6bd2239b9b73befc7005c46744b32
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-09-12 09:20:25 +0200

    Another state for BookEdit added, which is boolean. We need to show either the title of the book or BookEdit

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
 
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false); // we don't want to ShowEdit by default
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit)
+    }
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
     };
 
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit />;
+    }
+
+
     return <div className="book-show">
-        {book.title}
+        <div>{content}</div>
         <div className="actions">
-            <button className="delete" onClick={handleClick}>Delete</button>
+            <button className='edit' onClick={handleEditClick}>Edit</button>
+            <button className="delete" onClick={handleDeleteClick}>Delete</button>
         </div>
     </div>
 }
 
 export default BookShow;
+
+// need to show either:
+// the TITLE of the book
+// or
+// BOOKEDIT.
+
+// Content is changing, when user clicks the pensil icon,
+// so we need to use state.
