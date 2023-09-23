012138ec7a3ca52ee67f7d1a2cca82b724af81a6
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2023-09-23 22:07:35 +0200

    added useContext as a test

--- README.md
+++ README.md
@@ -212,5 +212,239 @@ const root = ReactDOM.createRoot(el);
 
 root.render(<App />);
 
+// BAD SOLUTION to update the state of books:
+
+// const createBook = (title) => {
+//     books.push({ id: 123, title: title });
+//     setBooks(books);
+// }
+
+// initially books.length is equal 0, but after adding some title nothing changes!
+
+// Before rerendering our application, React is going to look at its reference to the cuurent state of books and compare it to the new state of books.
+// If the reference is the same, React will not rerender the application.
+// This is why we need to create a new array of books and set that as the new state of books.
+
+// GOOD SOLUTION TO UPDATE AN ARRAY OR OBJECT (managed by our state system):
+
+//  const createBook = (title) => {
+//      const newBooks = [...books, { id: 123, title: title }];
+//      setBooks(newBooks);
+//  }
+
+//  so...
+
+//  1. Create a new array
+//  2. Copy all of the elements from the old array into the new array
+//  3. Add the new element to the new array
+
+//  If there is an array or object, you have to be very careful about how you update this piece of state!
+
+// ============================================================================================================ //
+
+// ADDING elements to the START of an array:
+
+//  const [colors, setColors] = useState([]);
+
+// const addColor = (newColor) => {
+//    const updatedColors = [newColor, ...colors];
+//    setColors(updatedColors);
+// }
+
+// ============================================================================================================ //
+
+// ADDING elements to the END of an array:
+
+//  const [colors, setColors] = useState([]);
+
+// const addColor = (newColor) => {
+//    const updatedColors = [...colors, newColor];
+//    setColors(updatedColors);
+// }
+
+// ============================================================================================================ //
+
+// ADDING elements to the MIDDLE of an array:
+
+//  const [colors, setColors] = useState([]);
+
+// const addColor = (newColor, index) => {
+//    const updatedColors = [
+//          ...colors.slice(0, index),
+//          newColor,
+//          ...colors.slice(index)];
+//    setColors(updatedColors);
+// }
+
+// ============================================================================================================ //
+
+// REMOVING an element with a particular value from an array:
+
+//  const [colors, setColors] = useState(['red', 'green', 'blue']);
+
+// const removeColor = (colorToRemove) => {
+//      const updatedColors = colors.filter((color) => {
+//          return color !== colorToRemove 
+//      });
+// setColors(updatedColors);
+// };
+
+// ============================================================================================================ //
+
+// REMOVING an element at a particular index from an array:
+
+//  const [colors, setColors] = useState(['red', 'green', 'blue']);
+
+// const removeColorAtIndex = (indexToRemove) => {
+//      const updatedColors = colors.filter((color, index) => {
+//          return indexToRemove !== index;
+//      });
+// setColors(updatedColors);
+// };
+
+// ============================================================================================================ // 
+
+// REMOVING an element at a particular property from an array:
+
+//  const [colors, setColors] = useState([
+//      { id: 1, name: 'Quo Vadis' },
+//      { id: 2, name: 'Pan Tadeusz' }
+//  ]);
+
+// const removeColorAtIndex = (idToRemove) => {
+//      const updatedColors = colors.filter((book) => {
+//          return idToRemove !== book.id;
+//      });
+// setColors(updatedColors);
+// };
+
+// FKT means Filter Keeps True (easier to remember);
+
+// ============================================================================================================ // 
+
+// MODIFYING an element based on a property:
+
+//  const [books, setBooks] = useState([
+//      { id: 1, title: 'Quo Vadis' },
+//      { id: 2, title: 'Pan Tadeusz' }
+//  ]);
+
+// const updateBookById = (id, newTitle) => {
+//      const updatedBooks = books.map((book) => {
+//          if (book.id === id) {
+//              return { ...book, title: newTitle };
+//          } 
+
+//          return book;
+//      });
+
+// setBooks(updatedBooks);
+// };
+
+// In this case the title in the book object is going to be ovewritten by the newTitle!
+
+// ============================================================================================================ //
+
+// ADDING AND CHANGING PROPERTIES in an OBJECT:
+
+// const [book, setBook] = useState({
+//     id: 1,
+//     title: 'Quo Vadis'
+// });
+
+// const updateBook = () => {
+    // const updatedBook = {           // create a new object
+        // ...book,                    // copy all of the properties from the book object
+        // title: 'Pan Tadeusz'        // add in your updated property (the old one will be overwritten)
+    // };    
+
+// setBook(updatedBook);
+// };
+
+// ============================================================================================================ //
+
+// REMOVING a property from an OBJECT:
+
+// const [book, setBook] = useState({
+//     id: 1,
+//     title: "Hobbit",
+//     author: "J.R.R.Tolkien"
+// });
+
+// const removedId = () => {
+//     const {id, ...rest} = book; // after ... use the name as you want
+
+//     setBook(rest);
+// }
+
+// ============================================================================================================ //
+
+// Props:
+// book - an object with id and title properties
+// onSubmit - a function that will be called when the user submits the form
+
+
+// ============================================================================================================ //
+
+//  useEffect
+
+//  1. This is a function that we import from React
+//  2. Used to run code when the component is INICIALLY rendered and sometimes when it is re-rendered
+//  3. First argument is a function that contains the code that you want to run
+//  4. Second argument is an array of dependencies or nothing - this controls whether or not the function is executed on re-renders.
+
+// Three tricky points about useEffect:
+
+// I. Understanding WHEN our arrow function is invoked
+// II. Understanding the arrow function's RETURN VALUE
+// III. Understanding state variable references
+
+// I. Understanding WHEN our arrow function is invoked
+
+// 1. Component called
+// 2. JSX is returned
+// 3. DOM is updated
+
+// Initial render is ended!
+// State is updated! (user made some changes)
+
+// 1. Component called
+// 2. JSX is returned
+// 3. DOM is updated
+
+// Second render is ended!
+// State is updated!
+
+// ... and so on
+// ... so when is our arrow function invoked?
+// it is automatically invoked immediately after the initial render 
+// and (maybe) after every re-render (second argument controls this).
+
+// EXAMPLES:
+
+    // useEffect(() => {
+    //     console.log("I am inside useEffect!");
+    // }, []);
+
+// called after inicial render
+// never called again
+
+  // useEffect(() => {
+    //     console.log("I am inside useEffect!");
+    // });
+
+// called after inicial render
+// also called after every re-render
+
+    // useEffect(() => {
+    //     console.log("I am inside useEffect!");
+    // }, [counter]);
+
+// called after inicial render
+// also called after every re-render if counter has changed (very rare)
+
+// To avoid mistakes with out of date state, we shoud take the response 
+// and use that to update our state instead of using the state directly (manually).
+
 
     
--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
 
     const fetchBooks = async () => {
         const response = await axios.get('http://localhost:3001/books');
-    
+
         setBooks(response.data);
     };
     // when should we invoke this function?
     useEffect(() => {
-        fetchBooks();  
+        fetchBooks();
     }, []);
 
     // DON'T DO THIS!!!
@@ -29,17 +29,17 @@ function App() {
     // }, []);
 
     const editBookById = async (id, newTitle) => {
-        
+
         const response = await axios.put(`http://localhost:3001/books/${id}`, {
             title: newTitle
         });
 
-    
+
         const updatedBooks = books.map((book) => {
-            if(book.id === id) {
-                return {...book, ...response.data}
+            if (book.id === id) {
+                return { ...book, ...response.data }
             }
-        return book; 
+            return book;
         });
 
         setBooks(updatedBooks);
@@ -62,258 +62,76 @@ function App() {
         // console.log(response);
 
         // console.log("I would like to read:", title);
-        const updatedBooks = [...books, 
-            response.data];
-            // {
-            // id: Math.round(Math.random() * 9999), // in some cases it may not be unique, but for small applications it is OK!
-            // title
+        const updatedBooks = [...books,
+        response.data];
+        // {
+        // id: Math.round(Math.random() * 9999), // in some cases it may not be unique, but for small applications it is OK!
+        // title
         // } // title: title
-        
+
         setBooks(updatedBooks)
-        };
-    
+    };
+
     return <div className="app">
         <h1>Reading List</h1>
-        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
+        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
         <BookCreate onCreate={createBook} />
     </div>
 }
 
 export default App;
 
-// BAD SOLUTION to update the state of books:
+// USING CONTEXT:
 
-// const createBook = (title) => {
-//     books.push({ id: 123, title: title });
-//     setBooks(books);
-// }
+// I. Create a context
 
-// initially books.length is equal 0, but after adding some title nothing changes!
+// 1. Creating a new file. Inside that file we will import createContext from react
+// 2. We wil receive a context object.
+// 3. This object has 2 properties (react components): Provider and Consumer (not very often used)
+// 4. To render the Provider we have to write something like:
+// <BooksContext.Provider>
 
-// Before rerendering our application, React is going to look at its reference to the cuurent state of books and compare it to the new state of books.
-// If the reference is the same, React will not rerender the application.
-// This is why we need to create a new array of books and set that as the new state of books.
 
-// GOOD SOLUTION TO UPDATE AN ARRAY OR OBJECT (managed by our state system):
+// II. Specify the data that will be shared
 
-//  const createBook = (title) => {
-//      const newBooks = [...books, { id: 123, title: title }];
-//      setBooks(newBooks);
-//  }
+// 'value' is a special prop that we can pass to the Provider:
 
-//  so...
+// Provider, that wraps the BooksContext.Provider component:
+// <BooksContext.Provider value={{count, incrementCount}}>
+//    <MyComponent />
+// </BooksContext.Provider>
+// The value can be any data type: string, number, array, object, function, etc.
 
-//  1. Create a new array
-//  2. Copy all of the elements from the old array into the new array
-//  3. Add the new element to the new array
+// e.g.:
+// const BooksContext = createContext();
 
-//  If there is an array or object, you have to be very careful about how you update this piece of state!
+// function Provider({ children }) {
+// const [count, setCount] = useState(0);
 
-// ============================================================================================================ //
-
-// ADDING elements to the START of an array:
-
-//  const [colors, setColors] = useState([]);
-
-// const addColor = (newColor) => {
-//    const updatedColors = [newColor, ...colors];
-//    setColors(updatedColors);
-// }
-
-// ============================================================================================================ //
-
-// ADDING elements to the END of an array:
-
-//  const [colors, setColors] = useState([]);
-
-// const addColor = (newColor) => {
-//    const updatedColors = [...colors, newColor];
-//    setColors(updatedColors);
-// }
-
-// ============================================================================================================ //
-
-// ADDING elements to the MIDDLE of an array:
-
-//  const [colors, setColors] = useState([]);
-
-// const addColor = (newColor, index) => {
-//    const updatedColors = [
-//          ...colors.slice(0, index),
-//          newColor,
-//          ...colors.slice(index)];
-//    setColors(updatedColors);
-// }
-
-// ============================================================================================================ //
-
-// REMOVING an element with a particular value from an array:
-
-//  const [colors, setColors] = useState(['red', 'green', 'blue']);
-
-// const removeColor = (colorToRemove) => {
-//      const updatedColors = colors.filter((color) => {
-//          return color !== colorToRemove 
-//      });
-// setColors(updatedColors);
+// const valueToShare = {
+//     count: count,
+//    incrementCount: () => {
+//        setCount(count + 1);
+//    }
 // };
 
-// ============================================================================================================ //
-
-// REMOVING an element at a particular index from an array:
-
-//  const [colors, setColors] = useState(['red', 'green', 'blue']);
-
-// const removeColorAtIndex = (indexToRemove) => {
-//      const updatedColors = colors.filter((color, index) => {
-//          return indexToRemove !== index;
-//      });
-// setColors(updatedColors);
-// };
-
-// ============================================================================================================ // 
-
-// REMOVING an element at a particular property from an array:
-
-//  const [colors, setColors] = useState([
-//      { id: 1, name: 'Quo Vadis' },
-//      { id: 2, name: 'Pan Tadeusz' }
-//  ]);
-
-// const removeColorAtIndex = (idToRemove) => {
-//      const updatedColors = colors.filter((book) => {
-//          return idToRemove !== book.id;
-//      });
-// setColors(updatedColors);
-// };
-
-// FKT means Filter Keeps True (easier to remember);
-
-// ============================================================================================================ // 
-
-// MODIFYING an element based on a property:
-
-//  const [books, setBooks] = useState([
-//      { id: 1, title: 'Quo Vadis' },
-//      { id: 2, title: 'Pan Tadeusz' }
-//  ]);
-
-// const updateBookById = (id, newTitle) => {
-//      const updatedBooks = books.map((book) => {
-//          if (book.id === id) {
-//              return { ...book, title: newTitle };
-//          } 
-
-//          return book;
-//      });
-
-// setBooks(updatedBooks);
-// };
-
-// In this case the title in the book object is going to be ovewritten by the newTitle!
-
-// ============================================================================================================ //
-
-// ADDING AND CHANGING PROPERTIES in an OBJECT:
-
-// const [book, setBook] = useState({
-//     id: 1,
-//     title: 'Quo Vadis'
-// });
-
-// const updateBook = () => {
-    // const updatedBook = {           // create a new object
-        // ...book,                    // copy all of the properties from the book object
-        // title: 'Pan Tadeusz'        // add in your updated property (the old one will be overwritten)
-    // };    
-
-// setBook(updatedBook);
-// };
-
-// ============================================================================================================ //
-
-// REMOVING a property from an OBJECT:
-
-// const [book, setBook] = useState({
-//     id: 1,
-//     title: "Hobbit",
-//     author: "J.R.R.Tolkien"
-// });
-
-// const removedId = () => {
-//     const {id, ...rest} = book; // after ... use the name as you want
-
-//     setBook(rest);
+// return (
+// <BooksContext.Provider value={valueToShare}>
+//    {children}
+// </BooksContext.Provider>
+// );
 // }
 
-// ============================================================================================================ //
+// III. 'Consume' the context in the components that need the data
 
-// Props:
-// book - an object with id and title properties
-// onSubmit - a function that will be called when the user submits the form
+// In order to access the data from the context we have to use the useContext hook:
+// import { useContext } from 'react';
+// import BooksContext from './book';
+
+// function MyComponent() {
+//     const books = useContext(BooksContext);
+
+//     return <div>{something}</div>
 
 
-// ============================================================================================================ //
-
-//  useEffect
-
-//  1. This is a function that we import from React
-//  2. Used to run code when the component is INICIALLY rendered and sometimes when it is re-rendered
-//  3. First argument is a function that contains the code that you want to run
-//  4. Second argument is an array of dependencies or nothing - this controls whether or not the function is executed on re-renders.
-
-// Three tricky points about useEffect:
-
-// I. Understanding WHEN our arrow function is invoked
-// II. Understanding the arrow function's RETURN VALUE
-// III. Understanding state variable references
-
-// I. Understanding WHEN our arrow function is invoked
-
-// 1. Component called
-// 2. JSX is returned
-// 3. DOM is updated
-
-// Initial render is ended!
-// State is updated! (user made some changes)
-
-// 1. Component called
-// 2. JSX is returned
-// 3. DOM is updated
-
-// Second render is ended!
-// State is updated!
-
-// ... and so on
-// ... so when is our arrow function invoked?
-// it is automatically invoked immediately after the initial render 
-// and (maybe) after every re-render (second argument controls this).
-
-// EXAMPLES:
-
-    // useEffect(() => {
-    //     console.log("I am inside useEffect!");
-    // }, []);
-
-// called after inicial render
-// never called again
-
-  // useEffect(() => {
-    //     console.log("I am inside useEffect!");
-    // });
-
-// called after inicial render
-// also called after every re-render
-
-    // useEffect(() => {
-    //     console.log("I am inside useEffect!");
-    // }, [counter]);
-
-// called after inicial render
-// also called after every re-render if counter has changed (very rare)
-
-// To avoid mistakes with out of date state, we shoud take the response 
-// and use that to update our state instead of using the state directly (manually).
-
-  
 
